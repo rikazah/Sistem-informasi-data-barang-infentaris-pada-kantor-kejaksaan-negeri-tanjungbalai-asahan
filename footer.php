97cb7a41187dc6dd21e2f9a0ca1d8b860d67ae6c
rikazah <46654114+rikazah@users.noreply.github.com>
2019-01-24 17:25:56 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: pink;
+  color: blue;
+  text-align: center;
+}
+
+</style>
+
+<div class="footer" id="user">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> SALAM DAMAI
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+           
+           <br>
+           <br>
+          <image src="img/logo.jpg" width="5%" height="5%">
+
+        </div>
+    </div>
+</div>
+
+
