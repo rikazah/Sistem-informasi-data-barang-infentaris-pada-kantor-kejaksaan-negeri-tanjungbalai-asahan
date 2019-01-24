f21db15f300e2531529ba63d6193920e678796d3
rikazah <46654114+rikazah@users.noreply.github.com>
2019-01-24 17:24:20 +0700

    Add files via upload

---
+++ views/about_tampil.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><span class="fa fa-user-plus"></span> About</h3>
+                </div>
+                <div class="panel-body">
+                    <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                        <thead>
+                            <p align="center"><img src="img/logo.jpg"></img></p>
+                            <label class="col-sm-12 control-label"><center><strong>SISTEM INFORMASI DATA BARANG INFENTARIS <br> KEJAKSAAN  NEGERI TANJUNG BALAI-ASAHAN</strong></center></label>
+                        </thead>
+                        <tbody>
+                          <div class="col-sm-2" align="center">
+
+                          </div>
+                          <div class="col-sm-8" align="center">
+                            <br>
+                            Sistem Informasi Data Barang Infentaris Negeri Tanjung Balai  merupakan sebuah sistem informasi yang dirancang untuk membantu untuk mempermudah pencatatan barang barang infentaris yang ada di kantor kejaksaan negeri tanjung balai-asahan, pencatatan nya  tidak secara manual lagi melainkan menggunakan komputerisasi agar data barang yang di input dapat tersimpan dengan akurat dan terjamin. 
+                           
+                            <br><br><br>
+                          </div>
+                          <div class="col-sm-2" align="center">
+                          </div>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
